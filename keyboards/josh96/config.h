7faa0ab64df99aa71ffdacbabb65968af4f97530
Josh Johnson <josh@joshajohnson.com>
2020-07-06 21:53:03 +1000

    update VID:PID

--- keyboards/josh96/config.h
+++ keyboards/josh96/config.h
@@ -4,8 +4,8 @@
 #include "config_common.h"
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0xFEED
-#define PRODUCT_ID      0x8f74
+#define VENDOR_ID       0x6A6A // JJ
+#define PRODUCT_ID      0x6A60 // J96
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    Josh Johnson
 #define PRODUCT         Keyboard
