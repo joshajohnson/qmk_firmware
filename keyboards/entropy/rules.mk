25430261a5d0d9d40b24fee8272f37346b152cdf
Josh Johnson <josh@joshajohnson.com>
2020-07-18 12:04:55 +1000

    moar progress

--- keyboards/entropy/rules.mk
+++ keyboards/entropy/rules.mk
@@ -15,6 +15,7 @@ SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no            # USB Nkey Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+WS2812_DRIVER = bitbang
 MIDI_ENABLE = no            # MIDI support
 BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = yes          # Audio output
