4e0c0ca4a8c67a9e7f49c0773b9c0af365081039
Josh Johnson <josh@joshajohnson.com>
2020-08-01 22:29:18 +1000

    audio not working

--- keyboards/chief/keymaps/led/keymap.c
+++ keyboards/chief/keymaps/led/keymap.c
@@ -15,14 +15,45 @@
  */
 #include QMK_KEYBOARD_H
 
+/* LED control */
+uint8_t status_en = 1;
+
+/* Jiggler */
+static uint32_t key_timer = 0;
+static bool dir = true;
+static bool en = false;
+
+enum custom_codes{
+    JIGGLER = SAFE_RANGE
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT(
-       RGB_TOG,    RGB_RMOD,    RGB_MOD,    KC_D,  \
-       KC_E,    KC_F,    KC_G,    KC_H,  \
+       RGB_TOG,    RGB_RMOD,    RGB_MOD,    JIGGLER,  \
+       CK_TOGG,    CK_UP,    CK_DOWN,    KC_H,  \
        KC_I,    KC_J,    KC_K,    KC_L  \
 )
-
 };
+
+/*
+Per Key Actions
+*/
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+
+    case JIGGLER:
+        if (record->event.pressed) {
+            en ^= 1;
+            if (en){
+            } else {
+            }
+            return true;
+        } else {
+        }
+        break;
+    }
+    return true;
+}
 
 void encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
@@ -49,5 +80,19 @@ void encoder_update_user(uint8_t index, bool clockwise) {
         } else {
             tap_code16(RGB_SPD);
         }
+    }
+}
+
+/*
+Mouse Jiggler
+*/
+void matrix_scan_user(void) {
+    if (timer_elapsed32(key_timer) > 100) {
+        key_timer = timer_read32();
+        if (en && dir)
+            tap_code(KC_MS_U);
+        else if (en && !dir)
+            tap_code(KC_MS_D);
+        dir ^= 1;
     }
 }
