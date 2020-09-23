a0f4a09e4707debb28e7284ddb09a3c5ba22ea28
Josh Johnson <josh@joshajohnson.com>
2020-09-23 18:59:09 +1000

    updated via config for mario

--- keyboards/macro4/keymaps/via/keymap.c
+++ keyboards/macro4/keymaps/via/keymap.c
@@ -16,24 +16,47 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes{
+    MACRO_1 = SAFE_RANGE,
+    MACRO_2,
+    MACRO_3,
+    MACRO_4
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_A, KC_B,
-    KC_C, KC_D
+    MACRO_1, MACRO_2,
+    MACRO_3, MACRO_4
   )
 };
 
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case MACRO_1:
+        if (record->event.pressed) {
+        SEND_STRING(SS_LCTL(SS_LSFT("M"))); // Mute Webex
+      }
+
+    case MACRO_2:
+        tap_code(KC_MPLY); // Play / Pause Music
+      break;
+
+    case MACRO_3:
+        SEND_STRING(SS_LCTL(SS_LALT("D"))); // Share Screen
+        break;
+
+    case MACRO_4:
+        SEND_STRING(SS_LCTL(SS_LALT("A"))); // Share Application
+        break;
+  }
+  return true;
+};
+
+// Encoder turns volume up / down
 void encoder_update_user(uint8_t index, bool clockwise) {
-    if (index == 0) { /* Left Encoder */
         if (clockwise) {
-            tap_code(KC_E);
+            tap_code(KC_VOLU);
         } else {
-            tap_code(KC_F);
+            tap_code(KC_VOLD);
         }
-    } else if (index == 1) { /* Right Encoder */
-        if (clockwise) {
-            tap_code(KC_G);
-        } else
-            tap_code(KC_H);
-        }
-}
+};
