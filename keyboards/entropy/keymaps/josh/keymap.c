d6ddd2cf6cc712c6bcaec17dbb69008dc3eb8a96
Josh Johnson <josh@joshajohnson.com>
2021-07-24 20:27:41 +1000

    clean up entropy keymap

--- keyboards/entropy/keymaps/josh/keymap.c
+++ keyboards/entropy/keymaps/josh/keymap.c
@@ -15,14 +15,12 @@
  */
 #include QMK_KEYBOARD_H
 
-#include "muse.h"
-
 // Useability defines
 #define ___ KC_NO
 #define KC_WRAP KC_F22
 
 // Shorter defs to fit in keymap
-#define MO_SPC_LOW LT(_LOWER, KC_SPACE)
+#define MO_SPC_RSE LT(_RAISE, KC_SPACE)
 #define MT_RA_M1 RALT_T(KC_MS_BTN1)
 #define MT_RC_APP RCTL_T(KC_APP)
 #define OSL_M OSL(_MACRO)
@@ -43,7 +41,6 @@ enum layer_names {
     _QWERTY,
     _NORMAL,
     _MACRO,
-    _LOWER,
     _RAISE,
     _FN
 };
@@ -51,14 +48,7 @@ enum layer_names {
 enum custom_codes{
     STAT_EN = SAFE_RANGE,
     UNDER_EN,
-    JIGGLER,
-    SNDSTRM,
-    USSR,
-    ALLSTR,
-    RCKRLL,
-    M_THM,
-    M_GMVR,
-    M_MSHRM
+    JIGGLER
 };
 
 /*
@@ -66,12 +56,12 @@ Keymap
 */
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_COLEMAK] = LAYOUT_2U_SS(
-    _______,LCTL(KC_W), KC_MUTE, KC_MPLY,  KC_ESC,   KC_F1,    KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12, KC_INS, KC_DEL,  KC_HOME,  \
-    KC_PMNS, KC_PAST,   KC_PSLS,   OSL_M,  KC_GRV,      KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS,  KC_EQL,    KC_BSPC,    KC_PGUP,  \
-               KC_P7,     KC_P8,   KC_P9,  KC_TAB,        KC_Q,    KC_W,    KC_F,    KC_P,   KC_G,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,   KC_LBRC, KC_RBRC,  KC_BSLS,    KC_PGDN,  \
+    _______,LCTL(KC_W), KC_MUTE, KC_MPLY,  KC_ESC,   KC_F1,    KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12, KC_INS, KC_DEL,  KC_HOME, \
+    KC_PMNS, KC_PAST,   KC_PSLS,   OSL_M,  KC_GRV,      KC_1,    KC_2,    KC_3,    KC_4,   KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,   KC_MINS,  KC_EQL,    KC_BSPC,    KC_PGUP, \
+               KC_P7,     KC_P8,   KC_P9,  KC_TAB,        KC_Q,    KC_W,    KC_F,    KC_P,   KC_G,    KC_J,    KC_L,    KC_U,    KC_Y, KC_SCLN,   KC_LBRC, KC_RBRC,  KC_BSLS,    KC_PGDN, \
     KC_PPLS,   KC_P4,     KC_P5,   KC_P6, KC_BSPC,         KC_A,    KC_R,   KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,   KC_QUOT,          KC_ENT,     KC_END,  \
                KC_P1,     KC_P2,   KC_P3, KC_LSFT,           KC_Z,    KC_X,  KC_C,    KC_V,    KC_B,    KC_K,    KC_M,  KC_COMM,   KC_DOT, KC_SLSH,       KC_RSFT,    KC_UP,    MO(_FN),  \
-    KC_PENT, KC_PDOT,        KC_P0,       KC_LCTL,  KC_LGUI,    KC_LALT,          MO_SPC_LOW,    KC_SPC,          MO(_RAISE),        MT_RA_M1,   MT_RC_APP,  KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_PENT, KC_PDOT,        KC_P0,       KC_LCTL,  KC_LGUI,    KC_LALT,          MO_SPC_RSE,    KC_SPC,          MO(_RAISE),        MT_RA_M1,   MT_RC_APP,  KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
     [_QWERTY] = LAYOUT_2U_SS(
@@ -80,7 +70,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                KC_P7,   KC_P8,   KC_P9,  KC_TAB,        KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_LBRC, KC_RBRC,  KC_BSLS,    KC_PGDN,  \
     KC_PPLS,   KC_P4,   KC_P5,   KC_P6,  KC_BSPC,         KC_A,    KC_S,   KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,  KC_SCLN,   KC_QUOT,         KC_ENT,     KC_END,  \
                KC_P1,   KC_P2,   KC_P3,  KC_LSFT,           KC_Z,    KC_X,  KC_C,    KC_V,    KC_B,    KC_N,    KC_M,  KC_COMM,   KC_DOT, KC_SLSH,       KC_RSFT,    KC_UP,    MO(_FN),  \
-    KC_PENT, KC_PDOT,      KC_P0,        KC_LCTL,  KC_LGUI,    KC_LALT,          MO_SPC_LOW,    KC_SPC,          MO(_RAISE),        MT_RA_M1,   MT_RC_APP,  KC_LEFT, KC_DOWN, KC_RIGHT
+    KC_PENT, KC_PDOT,      KC_P0,        KC_LCTL,  KC_LGUI,    KC_LALT,          MO_SPC_RSE,    KC_SPC,          MO(_RAISE),        MT_RA_M1,   MT_RC_APP,  KC_LEFT, KC_DOWN, KC_RIGHT
 ),
 
     [_NORMAL] = LAYOUT_2U_SS(
@@ -101,31 +91,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______,          _______, _______,   _______,    _______,         _______,       _______,      _______,        _______,              _______,  _______, _______, _______
 ),
 
-    [_LOWER] = LAYOUT_2U_SS(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
-    _______, _______, _______, TG(_MACRO), _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,     _______,       _______,  \
-             _______, _______, _______,  KC_ESC,     _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,    _______,   _______,  \
-    _______, _______, _______, _______,  KC_DEL,        KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, _______, _______,  KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,       _______,     _______,  \
-             _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,  KC_END, KC_HOME, _______, _______,      _______,     _______, _______,  \
-    _______, _______,          _______, _______,   _______,    _______,         _______,       _______,      _______,        _______,              _______,  _______, _______, _______
-),
-
     [_RAISE] = LAYOUT_2U_SS(
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
-    _______, _______, _______, KC_CAPS, _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,      _______,      _______,  \
-             _______, _______, _______,  KC_ESC,     _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______, _______, _______,    _______,   _______,  \
-    _______, _______, _______, _______,  KC_DEL,        KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,        _______,      _______,  \
-             _______, _______, _______, _______,          _______, _______, KC_BTN1, KC_BTN2, _______, _______,  KC_END, KC_HOME, _______, _______,      _______,     _______, _______,  \
+    _______, _______, _______, _______, _______, KC_F1,    KC_F2,   KC_F3,   KC_F4,  KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,   KC_F11,  KC_F12,      _______,      _______,  \
+             _______, _______, _______,  KC_ESC,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,   _______,  \
+    _______, _______, _______, _______,  KC_DEL,        KC_VOLD, KC_VOLU, KC_MPLY, KC_MPRV, KC_MNXT, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______,        _______,      _______,  \
+             _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______,      _______,    _______, _______,  \
     _______, _______,          _______, _______,   _______,    _______,         _______,       _______,      _______,        _______,              _______,  _______, _______, _______
 ),
 
     [_FN] = LAYOUT_2U_SS(
-    JIGGLER, _______, RGB_TOG, STAT_EN,    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   RESET,  \
-    CK_DOWN, _______, _______, KC_NLCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______,     TG(_QWERTY),  \
-              RCKRLL,   M_THM, _______,    _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,    _______,  TG(_NORMAL),  \
-    CK_UP,      USSR,  M_GMVR, _______,    _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,      _______,  \
-              ALLSTR, M_MSHRM, _______,    _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______, _______,  \
-    CK_TOGG, SNDSTRM,          _______,    _______,   _______,    _______,         _______,       _______,      _______,        _______,              _______,  _______, _______, _______
+    JIGGLER, _______, RGB_TOG, STAT_EN, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   RESET,  \
+    _______, _______, _______, KC_NLCK, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,     _______,  TG(_QWERTY),  \
+             _______, _______, _______, _______,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, TG(_NORMAL),  \
+    _______, _______, _______, KC_CAPS, _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,       _______,      _______,  \
+             _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,      _______,     _______, _______,  \
+    _______, _______,          _______, _______,   _______,    _______,         _______,       _______,      _______,        _______,              _______,  _______, _______, _______
 )
 
 };
@@ -200,8 +181,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 /*
-D
-
 Default:
 Enc 0: Disabled
 Enc 1: Scroll Up / Down
@@ -388,9 +367,11 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
         // Second LED
         switch (get_highest_layer(state)) {
+/*
         case _LOWER:
             rgblight_setrgb_at(0, 0, 64, LED1);
             break;
+*/
         case _RAISE:
             rgblight_setrgb_at(0, 64, 0, LED1);
             break;
